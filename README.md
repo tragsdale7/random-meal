4f382c3354d2f7f921b45373cc71884d1ca3c595
TJ Ragsdale <tragsdale7@gmail.com>
2019-02-27 17:06:02 -0500

    finish application

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Pulls random recipe from an API and displays the resulting information.
 
-Concepts: API, AJAX, higher order array methods, media queries.
+Concepts: API, AJAX, higher order array methods, media queries, flexbox, responsive design.
--- main.js
+++ main.js
@@ -1,62 +1,68 @@
-// dom elements
+// get dom elements
 let dom = {
 	startBtn: document.querySelector('.showcase-btn'),
-	name: document.querySelector('.meal-name'),
-	category: document.getElementById('category'),
-	ingredients: document.querySelector('.ingredients-list'),
-	instructions: document.querySelector('.instructions'),
-	img: document.querySelector('.meal-img')
-}
+	name: document.querySelector('.recipe__name'),
+	category: document.querySelector('.recipe__category'),
+	ingredients: document.querySelector('.recipe__ingredients'),
+	instructions: document.querySelector('.recipe__instructions'),
+	img: document.querySelector('.recipe__img')
+};
 
 // event listeners
-// dom.startBtn.addEventListener('click', e => {
-// 	console.log(e.target);
-// 	console.log(e)
-// })
+document.querySelector('.recipe__btn').addEventListener('click', sendAPIRequest);
 
-function sendRequest() {
+//send API request
+function sendAPIRequest() {
 	let xhr = new XMLHttpRequest();
 
 	xhr.open('GET', 'https://www.themealdb.com/api/json/v1/1/random.php', true);
 
-	xhr.onreadystatechange = function(){
-		if(this.readyState === 4 && this.status === 200){
-			let meal = JSON.parse(xhr.responseText).meals[0]
+	xhr.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200) {
+			let meal = JSON.parse(xhr.responseText).meals[0];
 			console.log(meal);
 			displayMeal(meal);
 		}
 	}
-
 	xhr.send();
 }
+sendAPIRequest();
 
-sendRequest();
-
-function displayMeal(meal){
+// update UI
+function displayMeal(meal) {
 	dom.name.textContent = meal.strMeal;
 	dom.category.textContent = meal.strCategory;
 	dom.instructions.textContent = meal.strInstructions;
 	dom.img.src = meal.strMealThumb;
-
-	function displayIngredients(meal) {
-		let ingredientNames = getIngredientComponents(meal, 'strIngredient');
-
-		let ingredientMeasures = getIngredientComponents(meal, 'strMeasure');
-
-		// display ingredient list to DOM
-		for(let i = 0; i < ingredientNames.length; i++){
-			let li = document.createElement('li');
-
-			li.textContent = `${ingredientMeasures[i]} ${ingredientNames[i]}`;
-
-			dom.ingredients.appendChild(li)
-		}
-		
-	}
-
-	// get ingredients and their measures
-	function getIngredientComponents(meal, component){
-		return Object.entries(meal).filter(key => key[0].includes(component) && key[1]).map(el => el[1]);
-	}
 	displayIngredients(meal);
+}
+
+function displayIngredients(meal) {
+	let ingredientNames = getIngredientComponents(meal, 'strIngredient');
+	let ingredientMeasures = getIngredientComponents(meal, 'strMeasure');
+
+	// reset the ingredients list
+	deleteIngredientNodes();
+
+	// add ingredients to UI
+	for (let i = 0; i < ingredientNames.length; i++) {
+		let li = document.createElement('li');
+		li.classList.add('recipe__ingredients-item')
+
+		li.textContent = `${ingredientMeasures[i]} ${ingredientNames[i]}`;
+
+		dom.ingredients.appendChild(li)
+	}
+}
+
+function deleteIngredientNodes() {
+	// loop through child nodes of ingredients list and delete them
+	while(dom.ingredients.firstChild) {
+		dom.ingredients.removeChild(dom.ingredients.firstChild);
+	}
+}
+
+// get unknown number of ingredients and their measures
+function getIngredientComponents(meal, component) {
+	return Object.entries(meal).filter(key => key[0].includes(component) && key[1]).map(el => el[1]);
 }
